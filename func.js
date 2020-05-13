ed1c8a1dcf15930450c2bd63da159dc8b8c67d02
atayab3 <59861431+atayab3@users.noreply.github.com>
2020-05-13 09:53:36 -0500

    Update func.js

--- func.js
+++ func.js
@@ -1,9 +1,3 @@
-// 	document.getElementById("getDecBtn").addEventListener("click", 
-// 														  () => 
-// 														  {posDecToBinary(decValue)}
-// 														 ) ;
-// 	<!--TESTING GIT HUB	 -->
-
 // Positive or Negative Decimal Number to Binary Number
 document.getElementById("getDecBtn").addEventListener("click", convertDecimaltoBinary ) ;
 
@@ -20,14 +14,8 @@ function validityCheck(inputVal){
 		document.body.appendChild(p1);
 		return true; 
 	}
-// 	if(inputVal.includes("[a-zA-Z]+")) { 
-// 		let p1 = document.createElement("p");
-// 		p1.textContent = "Please enter a valid value.";
-// 		document.body.appendChild(p1);
-// 		return true; 
-// 	  }
-}
 
+}
 
 // if length of binary string is not a multiple of 4, add correct number of 0s needed to get there
 function lengthenOutput(binaryArr){
@@ -78,7 +66,6 @@ function convertDecimaltoBinary(){
 	// Convert string to int
 	var inputValNum = Number(inputVal);
 	
-	
 	// Inputted DECIMAL value is positive
 	if(inputValNum >= 0){ 
 		var binaryArr = posDecToBinary(inputValNum, binary); //12
@@ -91,6 +78,10 @@ function convertDecimaltoBinary(){
 		// Output what the decimal converted to Binary will be
 	    let p1 = document.createElement("p");
 		p1.textContent = "+" + inputVal + " is " + stringbinary + " in binary.";
+		
+		
+		var e = document.getElementById("addValuesHere").querySelector("p"); 
+		document.getElementById("addValuesHere").removeChild(e);
 		document.getElementById("addValuesHere").appendChild(p1);
 
 	  }
@@ -109,20 +100,19 @@ function convertDecimaltoBinary(){
 		  
 		  let p2 = document.createElement("p");
 		  p2.textContent = inputVal + " is " + negStringBinary + " in Two's Complement binary";
+		  var e = document.getElementById("addValuesHere").querySelector("p"); 
+		  document.getElementById("addValuesHere").removeChild(e);
 		  document.getElementById("addValuesHere").appendChild(p2);	
 	  }
 	
 }
 
 function posDecToBinary(decValue, binary) {
-	var origValue = decValue;
+	let origValue = decValue;
 	
 	while(decValue > 0){
-		var Remainder = decValue % 2 ;
-
-// 	if(Remainder == 1 | Remainder == 0){
+		let Remainder = decValue % 2 ;
 		binary.unshift(Remainder);	
-// 	}
 		decValue = decValue / 2;
 		decValue = Math.floor(decValue);  
   }
@@ -134,7 +124,7 @@ function posDecToBinary(decValue, binary) {
 function negDecToBinary(binaryArr, negBinary){
 	
 	//Flipping bits of user inputted number ALREADY in positive binary
-	for(var i = 0 ; i < binaryArr.length ;  ++i ){
+	for(let i = 0 ; i < binaryArr.length ;  ++i ){
 		if(binaryArr[i] == 1){
 			negBinary.push(0); 
 		}
@@ -147,8 +137,8 @@ function negDecToBinary(binaryArr, negBinary){
 		negBinary.unshift(1);
 	}
 	// ADDING 1 to the lowest bit
-	var carryOver = 0 ;
-	for(var i =  negBinary.length -1 ; i > 0; --i  ){
+	let carryOver = 0 ;
+	for(let i =  negBinary.length -1 ; i > 0; --i  ){
 		if(i == negBinary.length-1){
 			if(negBinary[i] == 0 ){  
 				negBinary.splice(i, 1, 1); // add index i, remove 1 element, and add a one
@@ -180,18 +170,18 @@ function negDecToBinary(binaryArr, negBinary){
 // Used for POSITIVE binary to decimal conversions
 function convertPosBinToDecimal(){
 	// Selecting the input element and get its value
-	var decVal = 0; 
-    var binVal = document.getElementById("binaryVal").value;
+	let decVal = 0; 
+    let binVal = document.getElementById("binaryVal").value;
 	if(validityCheck(binVal)){
 		return;
 	}
-	var tempNum = 0; // so string can be converted to int
+	let tempNum = 0; // so string can be converted to int
 	
 	// remove all spaces from user input
 	binVal = spaceRemover(binVal);
 		
 	//traverse through
-	for(var i = 0 ; i < binVal.length ; ++i){
+	for(let i = 0 ; i < binVal.length ; ++i){
 		//convert char to an integer value
 		tempNum = Number(binVal.charAt(i) );
 		//Add correct powers of 2 based on placement on 1s in binary string
@@ -201,7 +191,9 @@ function convertPosBinToDecimal(){
 	//Create paragraph element containing the conversion to 
 	let p3 = document.createElement("p");
 	p3.textContent = binVal + " is " + decVal + " in decimal form.";
-	document.body.appendChild(p3);
+	var e = document.getElementById("addValuesHere2").querySelector("p"); 
+	document.getElementById("addValuesHere2").removeChild(e);
+	document.getElementById("addValuesHere2").appendChild(p3);
 	return decVal; 
 }
 
@@ -209,19 +201,19 @@ function convertPosBinToDecimal(){
 //Used for Twos Complemenmt conversion of binary to decimal
 function convertTwosComptoDecimal(){
 // 	var TCVal = document.getElementById("TwosCompVal").value;
-	var TCVal = document.getElementById("binaryVal").value;
+	let TCVal = document.getElementById("binaryVal").value;
 	if(validityCheck(TCVal)){
 		return;
 	}
 	// Selecting the input element and get its value
-	var decVal = 0; 
-	var tempNum = 0; // so string can be converted to int
+	let decVal = 0; 
+	let tempNum = 0; // so string can be converted to int
 
 	// remove all spaces from user input
 	TCVal = spaceRemover(TCVal);
 		
 	//traverse through - Adding correct powers of 2 based on placement on 1s in binary string
-	for(var i = 0 ; i < TCVal.length ; ++i){
+	for(let i = 0 ; i < TCVal.length ; ++i){
 		//convert char to an integer value
 		tempNum = Number(TCVal.charAt(i) );
 		   
@@ -237,6 +229,8 @@ function convertTwosComptoDecimal(){
 	//Create paragraph element containing the conversion to 
 	let p3 = document.createElement("p");
 	p3.textContent = "Twos comp: " + TCVal + " is " + decVal + " in decimal form.";
-	document.body.appendChild(p3);
+	var e = document.getElementById("addValuesHere2").querySelector("p"); 
+	document.getElementById("addValuesHere2").removeChild(e);
+	document.getElementById("addValuesHere2").appendChild(p3);
 	return decVal; 
 }
